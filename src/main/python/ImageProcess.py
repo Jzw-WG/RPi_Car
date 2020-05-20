293927b638adaa769bea42776b6a7955f815012b
JZW <qq710327563@126.com>
2020-05-20 14:29:08 +0800

    增加光流（无效）

--- src/main/python/ImageProcess.py
+++ src/main/python/ImageProcess.py
@@ -7,6 +7,7 @@ from PIL import Image
 import numpy as np
 import cv2
 import math
+import numpy
 from enum import Enum
 import QRCode as myqr
 
@@ -28,14 +29,43 @@ class ImageProcess:
         self.dst_image = None
         self.qrcode = None
 
+        self.old_src_image = self.src_image
+        self.old_gray_src_image = self.gray_src_image
+        self.old_dst_image = self.dst_image
+        self.point_image = None
+
+        self.optical_flow_image = None
+        """cv_threshold params
+        """
         self.cv_threshold_thresh = 220.0
         self.cv_threshold_maxval = 255.0
         self.cv_threshold_type = cv2.THRESH_BINARY
 
+        """opticalflow params
+        """
+        
+        # ShiTomasi 角点检测参数
+        self.feature_params = dict( maxCorners = 100,
+                            qualityLevel = 0.1,
+                            minDistance = 7,
+                            blockSize = 7 )
+
+        # lucas kanade光流法参数
+        self.lk_params = dict( winSize  = (15,15),
+                        maxLevel = 2,
+                        criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+        # 创建随机颜色
+        self.color = np.random.randint(0,255,(100,3))
+        self.p0 = None
+
+        """blur params
+        """
         self.blur_type = BlurType.Median_Filter
         self.blur_radius = 1.8018018018018018
 
     def image_process(self, input):
+        self.src_image = input
         self.gray_src_image = self.desaturate(self.src_image)
         self.threshold_image = self.cv_threshold(self.gray_src_image,self.cv_threshold_thresh,self.cv_threshold_maxval,self.cv_threshold_type)
         self.blur_image = self.blur(self.threshold_image, self.blur_type, self.blur_radius)
@@ -44,15 +74,6 @@ class ImageProcess:
 
         self.qrcode = self.dst_image
         # self.get_qrcode_from_image()
-
-    @staticmethod
-    def rotate(image,angle,center=None,scale=1.0):
-        (w,h) = image.shape[:]
-        if center is None:
-            center = (w//2,h//2)   
-        wrapMat = cv2.getRotationMatrix2D(center,angle,scale)    
-        return cv2.warpAffine(image,wrapMat,(h,w))
-
 
     def get_qrcode_from_image(self):
         #读取图片，灰度化
@@ -90,6 +111,39 @@ class ImageProcess:
         # cv2.imshow("rotateImg",rotateImg)
         # cv2.waitKey()
 
+    def optical_flow(self):
+
+        old_gray = ImageProcess.desaturate(self.old_src_image)
+
+        # 创建一个蒙版用来画轨迹
+        mask = np.zeros_like(self.old_src_image)
+
+        frame_gray = ImageProcess.desaturate(self.src_image) #灰度化
+
+        # 计算光流
+        p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, self.p0, None, **self.lk_params)
+        # 选取好的跟踪点
+        good_new = p1[st==1]
+        good_old = self.p0[st==1]
+
+        # 画出轨迹
+        for i,(new,old) in enumerate(zip(good_new,good_old)):
+            a,b = new.ravel()#多维数据转一维,将坐标转换后赋值给a，b
+            c,d = old.ravel()
+            mask = cv2.line(mask, (a,b),(c,d), self.color[i].tolist(), 2)#画直线
+            self.point_image = cv2.circle(self.src_image,(a,b),5, self.color[i].tolist(), -1)#画点
+        self.optical_flow_image = cv2.add(self.point_image,mask) # 将画出的线条进行图像叠加
+
+        # 更新上一帧的图像和追踪点
+        self.p0 = good_new.reshape(-1,1,2)
+
+    @staticmethod
+    def rotate(image,angle,center=None,scale=1.0):
+        (w,h) = image.shape[:]
+        if center is None:
+            center = (w//2,h//2)   
+        wrapMat = cv2.getRotationMatrix2D(center,angle,scale)    
+        return cv2.warpAffine(image,wrapMat,(h,w))
     
     @staticmethod
     def cv_threshold(src, thresh, max_val, type):
--- src/main/python/RPiAppliacation.py
+++ src/main/python/RPiAppliacation.py
@@ -1,36 +1,61 @@
 import QRCode as myqr
 import ImageProcess as imgp
 import cv2
+import numpy
+import os
+import CarControl as cc
 from PIL import Image
 
 def camera_app():
-    mq = myqr.QRCode()
-    imgpro = imgp.ImageProcess()
-    capture = cv2.VideoCapture(0)
+    my_qr = myqr.QRCode()
+    my_imgpro = imgp.ImageProcess()
+    my_car = cc.CarControl()
+    # capture = cv2.VideoCapture(0)
+    capture = cv2.VideoCapture(os.getcwd() + '/src/main/image/test.mp4')
+    # 第一帧
+    ret,old_frame = capture.read()
+    my_imgpro.src_image = old_frame
+    my_imgpro.old_src_image = old_frame
+    my_imgpro.old_gray_src_image = my_imgpro.desaturate(old_frame)
+    my_imgpro.image_process(old_frame)
+    my_imgpro.old_dst_image = my_imgpro.dst_image
+    #获取图像中的角点，返回到p0中
+    my_imgpro.p0 = cv2.goodFeaturesToTrack(my_imgpro.qrcode, mask = None, **my_imgpro.feature_params)
     i = 0
+    
     while(1):
         i = i + 1
         ret,frame = capture.read()
-        imgpro.src_image = frame
-        imgpro.image_process(imgpro.src_image)
-        
-        # cv2.imshow('1',imgpro.dst_image)
+        my_imgpro.image_process(frame)
+
+        # 光流
+        # imgpro.optical_flow()
+
+        # cv2.imshow('1',imgpro.optical_flow_image)
         # if cv2.waitKey(1) == ord('q'):
         #     break
 
         # cv2图转为Image图
-        # Img_dst_image = Image.fromarray(cv2.cvtColor(imgpro.qrcode,cv2.COLOR_BGR2RGB))
+        Img_dst_image = Image.fromarray(cv2.cvtColor(my_imgpro.qrcode,cv2.COLOR_BGR2RGB))
         # 不处理直接识别二维码
-        Img_dst_image = Image.fromarray(cv2.cvtColor(frame,cv2.COLOR_BGR2RGB))
-        data = mq.decode_qrcode_zxbar(Img_dst_image)
+        # Img_dst_image = Image.fromarray(cv2.cvtColor(frame,cv2.COLOR_BGR2RGB))
+        data,rect,polygon = my_qr.decode_qrcode_zbar(Img_dst_image)
+
+
+        # 更新上一帧的图像和追踪点
+        old_frame = frame.copy()
         print(i,':',data)
 
-        cv2.imshow('frame',frame)
+        
+        if rect != None and polygon != None:
+            draw_image = my_qr.bounding_qrcode_zbar(my_imgpro.src_image,rect,polygon)
+
+        cv2.imshow('frame',draw_image)
         if cv2.waitKey(1) == ord('q'):
             break    
 
 def image_app():
-    mq = myqr.QRCode()
+    my_qr = myqr.QRCode()
     imgpro = imgp.ImageProcess()       
     src_img = cv2.imread(myqr.QRCode.qrfile)
     
@@ -44,7 +69,7 @@ def image_app():
     # Img_dst_image = Image.fromarray(cv2.cvtColor(imgpro.qrcode,cv2.COLOR_BGR2RGB))
     # 不处理直接识别二维码
     Img_dst_image = Image.fromarray(cv2.cvtColor(src_img,cv2.COLOR_BGR2RGB))
-    data = mq.decode_qrcode_zxbar(Img_dst_image)
+    data = my_qr.decode_qrcode_zbar(Img_dst_image)
     print(data)
 
 if __name__ == '__main__':
