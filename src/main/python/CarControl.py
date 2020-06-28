eb1d8dcc73ce609e7d8de0144715672002703738
JZW <qq710327563@126.com>
2020-06-28 19:46:11 +0800

    Add GPIO output

--- src/main/python/CarControl.py
+++ src/main/python/CarControl.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python
 #-- coding:utf8 --
+import RPi.GPIO as GPIO
 class CarControl:
     MAX_LEFT_RIGHT_ANGLE = 45
     DEFAULT_RECT_HEIGHT_RATE = 0.3
     CENTER_RANGE = 0.05
-
+    channel_list = [17, 27, 22, 18, 23, 24, 25, 12]
     @staticmethod
     def get_left_right_command(rect_center, image_width):
         if rect_center < image_width/2 - image_width*CarControl.CENTER_RANGE:
@@ -36,7 +37,11 @@ class CarControl:
         param = distance
         return command,param
 
+    def initial():
+        GPIO.setup(chan_list, GPIO.OUT)
+
     def transfer_rect_to_control(self, rect, image_shape):
+        initial()
         if rect != None and image_shape != None:
             rect_left = rect.left
             rect_center = rect.left + rect.width/2
@@ -75,21 +80,46 @@ class CarControl:
 
     @staticmethod
     def turn_left(angle):
+        # GPIO 17 and 27 output 01 = left
+        GPIO.output(channel_list[0], GPIO.LOW)
+        GPIO.output(channel_list[1], GPIO.HIGH)
+        # GPIO 18,23,24,25,12 output = bin(angle)
+        GPIO.output(channel_list[4], int(bits[-5]))
+        GPIO.output(channel_list[5], int(bits[-4]))
+        GPIO.output(channel_list[6], int(bits[-3]))
+        GPIO.output(channel_list[7], int(bits[-2]))
+        GPIO.output(channel_list[8], int(bits[-1]))
         print('turn left' + ', angle：',angle)
 
     @staticmethod
     def turn_right(angle):
+        # GPIO 17 and 27 output 10 = right
+        GPIO.output(channel_list[0], GPIO.HIGH)
+        GPIO.output(channel_list[1], GPIO.LOW)
+        # GPIO 18,23,24,25,12 output = bin(angle)
+        GPIO.output(channel_list[4], int(bits[-5]))
+        GPIO.output(channel_list[5], int(bits[-4]))
+        GPIO.output(channel_list[6], int(bits[-3]))
+        GPIO.output(channel_list[7], int(bits[-2]))
+        GPIO.output(channel_list[8], int(bits[-1]))
         print('turn right' + ', angle：',angle)
 
     @staticmethod
     def go_forward(distance):
+        # GPIO 22 output 0 = forward
+        GPIO.output(channel_list[2], GPIO.LOW)
         print('go forward' + ', distance：',distance)
 
     @staticmethod    
     def go_backward(distance):
+        # GPIO 22 output 1 = backward
+        GPIO.output(channel_list[2], GPIO.HIGH)
         print('go backward' + ', distance：',distance)
     
     @staticmethod    
     def stop():
+        # GPIO 17 and 27 output 11 = stop
+        GPIO.output(channel_list[0], GPIO.HIGH)
+        GPIO.output(channel_list[1], GPIO.HIGH)
         print('stop')
     
